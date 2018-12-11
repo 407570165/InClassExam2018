f8e45a4d2661b27e5274bdb30c790a4038b5dc64
user <user@es302n02>
2018-12-11 12:16:42 +0800

    ghghfh

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -1,5 +1,7 @@
 package com.exam;
 
 public class Order {
-
+	ArrayList bag =new ArrayList();
+	public Order(){
+		Ticket ticket=new Ticket(String Taipei,String Keelung,int price,int num,int total);
 }
--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -1,5 +1,16 @@
 package com.exam;
 
 public class Ticket {
-
+	String Taipei ;
+	String Keelung;
+	String TaiChung;
+	int price ;
+	int num;
+	int total;
+	public Ticket(String Taipei,String Keelung,int price,int num,int total){
+		this.Taipei=Taipei;
+		this.Keelung=Keelung;
+		this.price=price;
+		this.num=num;
+		this.total=total;
 }
