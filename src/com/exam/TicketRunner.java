9a4bf69afc339ddcaf7471bcd200360f92bc0336
user <user@es302n02>
2018-12-11 12:07:26 +0800

    initial

--- src/com/exam/TicketRunner.java
+++ src/com/exam/TicketRunner.java
@@ -1,19 +1,22 @@
 package com.exam;
 
 public class TicketRunner {
-	/*  Result:
-	 *  Taipei    Keelung  37   2  74
-	 *  TaiChung  Taipei   285  1  285
-	 * 
-	 */
+
 	public static void main(String[] args) {
-		Order order = new Order();
-		//origin, destination, price, quantity
-		Ticket ticket = new Ticket("Taipei", "Keelung", 37, 2);
-		order.add(ticket);
-		ticket = new Ticket("TaiChung", "Taipei", 285, 1);
-		order.add(ticket);
-		order.print();
+		// TODO Auto-generated method stub
+		/*  Result:
+		 *  Taipei    Keelung  37   2  74
+		 *  TaiChung  Taipei   285  1  285
+		 * 
+		 */
+		public static void main(String[] args) {
+			Order order = new Order();
+			//origin, destination, price, quantity
+			Ticket ticket = new Ticket("Taipei", "Keelung", 37, 2);
+			order.add(ticket);
+			ticket = new Ticket("TaiChung", "Taipei", 285, 1);
+			order.add(ticket);
+			order.print();
 	}
 
 }
